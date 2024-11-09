99a0d03b7b2ad03f5d2424ebb405a36c97b18020
DickermanJosh <123334042+DickermanJosh@users.noreply.github.com>
2024-11-09 12:58:38 -0800

    Disable skreecher object when in disabled state

--- Assets/_Scripts/Enemies/Skreecher/State/SkreecherDisabledState.cs
+++ Assets/_Scripts/Enemies/Skreecher/State/SkreecherDisabledState.cs
@@ -9,8 +9,13 @@ namespace _Scripts.Enemies.Skreecher.State
         public void EnterState(SkreecherStateManager enemy)
         {
             _enemy = enemy;
-            Physics2D.IgnoreCollision(_enemy.Collider2D, PlayerVariables.Instance.Collider2D);
-            _enemy.Collider2D.enabled = false;
+            // Physics2D.IgnoreCollision(_enemy.Collider2D, PlayerVariables.Instance.Collider2D);
+            // _enemy.Collider2D.enabled = false;
+            
+            // TODO: Eventually I think a teleport or poof effect on the skreecher would look good as the disabled anim?
+            // Leaving it hanging on the ceiling doesn't seem right, neither does letting it fall. TP away seems like the best solution.
+            // Maybe once you disable a skreecher it just teleports to a new position instead of disappearing?
+            _enemy.gameObject.SetActive(false);
         }
 
         public void UpdateState() {}
